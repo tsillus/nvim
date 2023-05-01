635ec609189f5cb6ab0bff81c0ca90265640d719
torsten.sillus <torsten.sillus@infranaut.eu>
2023-05-01 14:02:57 +0200

    custom neo-tree binds

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,51 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+        window = {
+
+            mappings = {
+                ["<space>"] = {
+                    "toggle_node",
+                    nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
+                },
+                ["<2-LeftMouse>"] = "open",
+                ["l"] = "open",
+                ["<esc>"] = "revert_preview",
+                ["P"] = { "toggle_preview", config = { use_float = true } },
+                --["l"] = "focus_preview",
+                ["S"] = "open_split",
+                -- ["S"] = "split_with_window_picker",
+                ["s"] = "open_vsplit",
+                -- ["s"] = "vsplit_with_window_picker",
+                ["t"] = "open_tabnew",
+                -- ["<cr>"] = "open_drop",
+                -- ["t"] = "open_tab_drop",
+                ["w"] = "open_with_window_picker",
+                ["C"] = "close_node",
+                ["z"] = "close_all_nodes",
+                --["Z"] = "expand_all_nodes",
+                ["R"] = "refresh",
+                ["a"] = {
+                    "add",
+                    -- some commands may take optional config options, see `:h neo-tree-mappings` for details
+                    config = {
+                        show_path = "none", -- "none", "relative", "absolute"
+                    },
+                },
+                ["A"] = "add_directory", -- also accepts the config.show_path and config.insert_as options.
+                ["d"] = "delete",
+                ["r"] = "rename",
+                ["y"] = "copy_to_clipboard",
+                ["x"] = "cut_to_clipboard",
+                ["p"] = "paste_from_clipboard",
+                ["c"] = "copy", -- takes text input for destination, also accepts the config.show_path and config.insert_as options
+                ["m"] = "move", -- takes text input for destination, also accepts the config.show_path and config.insert_as options
+                ["e"] = "toggle_auto_expand_width",
+                ["q"] = "close_window",
+                ["?"] = "show_help",
+                ["<"] = "prev_source",
+                [">"] = "next_source",
+            },
+        },
+    },
+}
