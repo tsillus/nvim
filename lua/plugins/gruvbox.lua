1dd236f2d2fd848a67e2e466b42b24c742b7ee5d
torsten.sillus <torsten.sillus@infranaut.eu>
2023-07-06 18:33:46 +0200

    fixed pwsh window color issue

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,3 +1,5 @@
+vim.o.background = "dark"
+
 return {
 
     {
@@ -18,13 +20,25 @@ return {
             invert_signs = false,
             invert_tabline = true,
             invert_intend_guides = false,
-            inverse = true, -- invert background for search, diffs, statuslines and errors
+            inverse = false, -- invert background for search, diffs, statuslines and errors
             contrast = "", -- can be "hard", "soft" or empty string
             palette_overrides = {},
             overrides = {},
-            dim_inactive = true,
+            dim_inactive = false,
             transparent_mode = false,
         },
+        opts = function()
+            local config = require("gruvbox").config
+            local colors = require("gruvbox.palette").get_base_colors(vim.o.background, config.contrast)
+
+            -- vim.g.terminal_color_0 = colors.bg0
+            -- vim.g.terminal_color_7 = colors.fg4
+            -- vim.g.terminal_color_15 = colors.fg1
+
+            vim.g.terminal_color_0 = colors.fg0
+            vim.g.terminal_color_7 = colors.bg4
+            vim.g.terminal_color_15 = colors.bg1
+        end,
     },
 
     {
